84b861bc0c3ff3b9b64ca190b355747f6f48bf45
Michael Boroski <mjboroski@gmail.com>
2018-04-26 21:25:47 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,6 +27,7 @@ class Scraper
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
+
   end
 
   def get_courses
@@ -41,4 +42,5 @@ class Scraper
       course.description = post.css("p").text
     end
   end
+
 end
